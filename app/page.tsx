142d56ba058490ed930f50dc9ce5c7874f59cb53
Amr Mohamed <amrmohamed.amaa@gmail.com>
2026-06-10 08:09:33 +0300

    02 Cutomization

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button variant='elevated'>Click me</Button>
+      <Input placeholder='Enter your name' />
+      <Progress value={50} />
     </div>
   );
 }
